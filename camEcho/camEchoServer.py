f19d5974b044a3140d9ceaaa658d6879d2adda47
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2021-03-15 21:57:03 +0800

    removed the not needed part of the code.

--- camEcho/camEchoServer.py
+++ camEcho/camEchoServer.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python
 #-----------------------------------------------------------------------------
-# Name:        cameraServer.py
+# Name:        cameraEchoServer.py
 #
-# Purpose:     This module will create a camera viewer server to connect to the 
-#              <camClient> by UDP client, get the camera video and do the motion 
-#              detection and simple target tracking.
+# Purpose:     This module will create a camera/video viewer which used to test 
+#              the image/data tranfer latency between 2 nodes in a network. It 
+#              will display the camera view/video in one window the send the data
+#              to the other side of the network. The other side <camEchoClient.py>
+#              will echo send the data back, then the sent back data will show 
+#              on another window.
 #              
 # Author:       Yuancheng Liu
 #
-# Created:     2020/03/16
-# Copyright:   YC @ Singtel Cyber Security Research & Development Laboratory
-# License:     YC
+# Created:     2021/03/11
+# Copyright:   Singtel Cyber Security Research & Development Laboratory
+# License:     N.A
 #-----------------------------------------------------------------------------
 
 import sys
@@ -23,7 +26,7 @@ import cv2
 import numpy as np
 import udpCom
 # image transfer : https://gist.github.com/kittinan/e7ecefddda5616eab2765fdb2affed1b
-
+TEST_MD = True  # Test mode: True - use pre-saved video, False - capture from camera.
 UDP_PORT = 5005
 BUFFER_SZ = udpCom.BUFFER_SZ
 CONFIG_FILE = 'camServerConfig.txt'
@@ -37,19 +40,20 @@ class camServer(object):
         """
         self.paramDict = self.loadConfig()
         self.client = udpCom.udpClient((self.paramDict['IPADD'], UDP_PORT))
-        self.staticBack = None 
         self.termiate = False   # program terminate flag
-        # Video capture part:
+        # self.staticBack = None # parameter used for 
+        # Video capture section:
         self.videoSrc = videoSrc
         print("Capture video from src: %s" % str(self.videoSrc))
         self.cam = cv2.VideoCapture(self.videoSrc)
         self.encodeParam = [int(cv2.IMWRITE_JPEG_QUALITY), 90] # image encode parameter
-        #self.setResolution(640, 480)
-        self.imageW = 1600
-        self.imageH = 900
-        self.setResolution(self.imageW, self.imageH)
-        self.data = None    # image data.
-        self.imageSz = 0    # image size 
+        # Image resolution setup:
+        self.imageW = self.cam.get(3) if TEST_MD else 1600
+        self.imageH = self.cam.get(4) if TEST_MD else 900
+        print("Video resolution is: %s" %str((self.imageW, self.imageH)))
+        if not TEST_MD: self.setResolution(self.imageW, self.imageH)
+        # Data parameters:
+        self.data = None        # image data (Byte data).
         self.now = time.time()  # time tag used to calculate the latency.
 
 #-----------------------------------------------------------------------------
@@ -75,11 +79,9 @@ class camServer(object):
     def run(self):
         """ main loop to fetch image bytes from the camera client."""
         print("Start to fetch the image.")
-        imgData = b'' # imagedata
         while not self.termiate:
             self.now = time.time()
-            # get a new image from the camera
-            imgData = b''
+            # get a new image from the camera/video
             _, image = self.cam.read()
             if image is None:
                 print("Reload the image source.")
@@ -90,15 +92,14 @@ class camServer(object):
                 _, image = self.cam.read()
             _, frame = cv2.imencode('.jpg', image, self.encodeParam)
             self.data = pickle.dumps(frame, 0)
-            frame = cv2.imdecode(pickle.loads(
-                self.data, fix_imports=True, encoding="bytes"), cv2.IMREAD_COLOR)
+            # Show the captured image in the Orignal window.
+            frame = cv2.imdecode(pickle.loads(self.data, fix_imports=True, encoding="bytes"), cv2.IMREAD_COLOR)
             cv2.imshow('Orignal capture',frame) # Show the orignal image.
-            
+            # Send the image data to the echo client.
             imgSz = len(self.data)
-            self.imageSz = imgSz
-
             rcvIterN = math.ceil(imgSz/float(BUFFER_SZ)) #iteration time to receive one img.
             print('Next image size: %s' %str(imgSz))
+            imgData = b'' # received image data.
             for _ in range(int(rcvIterN)):
                 msg = None
                 if len(self.data) > BUFFER_SZ:
@@ -112,22 +113,10 @@ class camServer(object):
             if imgSz != len(imgData):
                 print("Error: some image byte lose!")
                 continue
-            
+            # Show the echo sent back image on Window2.
             frame = cv2.imdecode(pickle.loads(
                 imgData, fix_imports=True, encoding="bytes"), cv2.IMREAD_COLOR)
-            
-            timeInterval = time.time() - self.now
-            dataRate = (self.imageSz/timeInterval)//1000
-            Imglatency = timeInterval*1000/2
-            datalatency = Imglatency/rcvIterN
-            cv2.rectangle(frame, (40, 600), (550, 720), (0, 0, 0), -1)
-
-            cv2.putText(frame,"ImgLatency[ms]: %s" %str(Imglatency) , (50,620), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), thickness = 2)
-            cv2.putText(frame,"DataLatency[ms]: %s" % str(datalatency) , (50 ,660), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), thickness = 2)
-            cv2.putText(frame,"DataRate[kbps]: %s" % str(dataRate) , (50 ,700), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), thickness = 2) 
-            
-            cv2.imshow('Echo Back Image',frame)
-
+            self.showEchoImage(frame, imgSz, rcvIterN)
             # Tempory disabled the detection.
             #self.detectTgt(frame)
 
@@ -139,6 +128,25 @@ class camServer(object):
         cv2.destroyAllWindows() 
 
 #-----------------------------------------------------------------------------
+    def showEchoImage(self, frame, imgSz, rcvIterN):
+        """ Show the echo sent back image in another window.
+        Args:
+            frame ([type]): cv image fram
+            imgSz ([type]): image size
+            rcvIterN ([type]): how many section the image was splitted for sending.
+        """
+        timeInterval = time.time() - self.now
+        dataRate = (imgSz/timeInterval)//1000
+        Imglatency = timeInterval*1000/2
+        datalatency = Imglatency/rcvIterN
+        yPos = 30 
+        cv2.rectangle(frame, (20, yPos), (550, yPos+120), (0, 0, 0), -1)
+        cv2.putText(frame,"ImgLatency[ms]: %s" %str(Imglatency) , (30,yPos+20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), thickness = 2)
+        cv2.putText(frame,"DataLatency[ms]: %s" % str(datalatency) , (30 ,yPos+60), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), thickness = 2)
+        cv2.putText(frame,"DataRate[kbps]: %s" % str(dataRate) , (30 ,yPos+100), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), thickness = 2)
+        cv2.imshow('Echo Back Image',frame)
+
+#-----------------------------------------------------------------------------
     def setResolution(self, w, h):
         """ Set the feed back image resulotion. """
         self.cam.set(3, w)
@@ -146,7 +154,8 @@ class camServer(object):
 
 #-----------------------------------------------------------------------------
     def detectTgt(self, frame):
-        """ Motion detection and target tracking function. Result will show in 
+        """ [Currently not used in this program]
+            Motion detection and target tracking function. Result will show in 
             opencv default window.
             reference: https://www.geeksforgeeks.org/webcam-motion-detector-python/
         """
@@ -174,10 +183,6 @@ class camServer(object):
                 # Making green rectangles around the moving object.
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3) 
         # Show the difference compare frame or the detection tracking frame.
-        timeInterval = time.time() - self.now
-        dataRate = (self.imageSz/timeInterval)//1000
-        cv2.putText(frame,"Latency[s]: %s, DataRate[kbps]: %s" %(str(timeInterval), str(dataRate)) , (100,100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), thickness = 1) 
-
         if self.paramDict['DISMD']:
             cv2.imshow('Different Gray-Scale Image',diffFrame)
         else:
@@ -186,7 +191,8 @@ class camServer(object):
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 def main():
-    cam = camServer(None)
+    videoSrc = 'test.mp4' if TEST_MD else 0 
+    cam = camServer(None, videoSrc=videoSrc)
     cam.run()
 
 #-----------------------------------------------------------------------------
